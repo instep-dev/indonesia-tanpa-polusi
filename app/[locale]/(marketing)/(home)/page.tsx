23a3b599a7dc74e549626d9ffdf80657e7b1c32c
Fadlan Daris <fadlandariss@gmail.com>
2026-07-27 21:23:57 +0700

    feat: compose Home page from marketing sections

--- app/[locale]/(marketing)/(home)/page.tsx
+++ app/[locale]/(marketing)/(home)/page.tsx
@@ -1,5 +1,10 @@
 import { getDictionary, type Locale } from '@/i18n/getDictionary'
 import Hero from '@/components/marketing/Hero'
+import AboutUs from '@/components/marketing/AboutUs'
+import OurStories from '@/components/marketing/OurStories'
+import LatestNews from '@/components/marketing/LatestNews'
+import ContactUs from '@/components/marketing/ContactUs'
+import { homeStoryCards, homeNewsCards, contactInfo } from '@/data/data'
 
 const Home = async ({
   params,
@@ -9,11 +14,15 @@ const Home = async ({
   const { locale } = await params
   const validLocale: Locale = locale === 'en' ? 'en' : 'id'
   const dict = await getDictionary(validLocale)
-  const dictData = dict.marketing.home
+  const homeDict = dict.marketing.home
 
   return (
     <div>
-      <Hero dict={dictData} />
+      <Hero dict={homeDict} />
+      <AboutUs dict={homeDict.about} />
+      <OurStories dict={homeDict.ourStories} cards={homeStoryCards} />
+      <LatestNews dict={homeDict.news} items={homeNewsCards} />
+      <ContactUs dict={homeDict.contact} contact={contactInfo} />
     </div>
   )
 }
